932d376fbc7779fdbefdaf2a9f0e44694861b995
khadijara <91148260+khadijara@users.noreply.github.com>
2022-06-15 15:58:28 +0200

    adding facebook social

--- website/src/components/Footer/FooterArray.js
+++ website/src/components/Footer/FooterArray.js
@@ -39,5 +39,10 @@ export const FooterArrayCl3 = [
         id: 1,
         title: 'Instagram',
         url: 'https://www.instagram.com/offworldturing/',
+    },
+    {
+        id: 2,
+        title: 'Facebook',
+        url: 'https://www.facebook.com/profile.php?id=100082365518622',
     }
 ]
